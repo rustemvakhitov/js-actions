9bb4d6def3c377e590344462727d92408e0dfdc3
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-05-05 14:54:53 -0600

    added logging

--- dist/index.js
+++ dist/index.js
@@ -31836,10 +31836,15 @@ async function run()
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const oktokit = github.getOctokit(GITHUB_TOKEN);
 
+    console.log('GITHUB_TOKEN == ' + GITHUB_TOKEN);
+
     const {context = {}} = github;
     const {pull_request} = context.payload;
 
     const repoName = context.payload.repository.name;
+
+    console.log('context.repo == ' + context.repo);
+    console.log('pull_request.number == ' + pull_request.number);
 
     await oktokit.issues.createComment({
         repo: context.repo, 
