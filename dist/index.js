2d6469ee00ff6948926548c39df80bf087012f13
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-05-05 18:00:17 -0600

    more params

--- dist/index.js
+++ dist/index.js
@@ -31839,8 +31839,8 @@ async function run()
 
     
 
-    //const {context = {}} = github;
-    const {pull_request} = context.payload;
+    const context = github.context;
+    const pull_request = context.payload.pull_request;
 
     const repoName = github.context.payload.repository.name;
 
@@ -31851,8 +31851,10 @@ async function run()
     console.log('context.repoName == ' + context.repoName);
     console.log('repoName == ' + repoName);
 
-    await oktokit.rest.issues.createComment({
+    await oktokit.issues.createComment({
         ...context.repo, 
+        owner: context.repo.owner,
+        repo: context.payload.repository.full_name,
         issue_number: pull_request.number, 
         body: 'Comment from RV'});
 
