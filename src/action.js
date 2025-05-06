621097abddca379cf3f5ab4da77b65ca7bad373f
rustemvakhitov <rustemvakhitov@hotmail.com>
2025-05-05 18:10:04 -0600

    c+P

--- src/action.js
+++ src/action.js
@@ -1,35 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const { context } = require('@actions/github')
+const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const octokit = github.getOctokit(GITHUB_TOKEN);
 
-async function run()
-{
-    console.log('Hello there!');
+const { pull_request } = context.payload;
 
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const oktokit = github.getOctokit(GITHUB_TOKEN);
-
-    
-
-    const context = github.context;
-    const pull_request = context.payload.pull_request;
-
-    const repoName = github.context.payload.repository.name;
-
-    console.log('GITHUB_TOKEN == ' + GITHUB_TOKEN);
-    console.log('context.repo == ' + context.repo);
-    console.log('github.context.payload.repository.name == ' + github.context.payload.repository.name);
-    console.log('pull_request.number == ' + pull_request.number);
-    console.log('context.repoName == ' + context.repoName);
-    console.log('repoName   == ' + repoName);
-
-    await oktokit.issues.createComment({
-        //...context.repo, 
-        owner: context.repo.owner,
-        repo: context.payload.repository.full_name,
-        issue_number: pull_request.number, 
-        body: 'Comment from RV'});
-
+async function run() {
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: 'comment'
+  });
 }
 
 run();
